b8237fc924cf82e4a2b39b301720daaf4e908aa9
Ajit Mistry <mail@a-mistry.com>
2024-10-17 21:31:25 +0200

    preprocessing

--- src/cuda_hgemm/src/mma/mmaST.cu
+++ src/cuda_hgemm/src/mma/mmaST.cu
@@ -19,9 +19,99 @@
 // 1 - 2:4 sparse block
 // 2 - dense block
 
-__global__ void mmaSTKernelSparse(half *bcsrValuesA, half *B, half *C, size_t M,
-                                  size_t N, size_t K, size_t nonzeroBlocks,
-                                  int *blockInfo,
+__global__ void
+preprocessing_mmaSTKernelSparse(half *bcsrValuesA, char *metadata,
+                                half *sparseMatrixA, size_t M, size_t N,
+                                size_t K, size_t nonzeroBlocks, int *blockInfo,
+                                int *relativeBlockIndexMapping) {
+  int PRINT_THREAD_ID = 11;
+  // mmaSTKernel
+  const size_t K_tiles = div_ceil(K, MMA_K);
+
+  const size_t warp_row = blockIdx.y * MMA_M;
+  const size_t warp_col = blockIdx.x * MMA_N;
+
+  size_t blockRow = blockIdx.y;
+  size_t blockCol = blockIdx.x;
+
+  size_t colRegions = (K + MMA_K - 1) / (MMA_K);
+
+  size_t blockIndex = blockRow * colRegions + blockCol;
+
+  if (warp_row >= M || warp_col >= N) {
+    return;
+  }
+
+  const size_t lane_id = threadIdx.x % WARP_SIZE;
+
+  __shared__ half C_smem[MMA_M][MMA_N];
+
+  uint32_t RC[2] = {0, 0};
+
+  // print K_tiles
+  DEBUG_PRINT_THREAD(PRINT_THREAD_ID, "K_tiles: %d\n", K_tiles);
+
+#pragma unroll
+  for (size_t i = 0; i < K_tiles; ++i) {
+    size_t blockIndex = blockRow * colRegions + i;
+    int sparsityInfo = blockInfo[blockIndex];
+    if (blockInfo[blockIndex] == 0) {
+      continue;
+    }
+    size_t relativeIndex = relativeBlockIndexMapping[blockIndex];
+
+    if (sparsityInfo == 2) {
+
+    }
+
+    else if (sparsityInfo == 1) {
+
+      __shared__ half A_smem[MMA_M][MMA_K / 2];
+      __shared__ half B_smem[MMA_N][MMA_K];
+      __shared__ char Meta_smem[MMA_M][MMA_K / 8];
+
+      half *src = // length 8
+          ((half *)((int4 *)(&bcsrValuesA[(relativeIndex)*MMA_M * MMA_K +
+                                          (lane_id / 2) * MMA_K]) +
+                    lane_id % 2));
+
+      half src_sparse[4];
+
+      char *cur_meta = (Meta_smem[lane_id / 2]) + (lane_id % 2);
+
+      *cur_meta = 0;
+
+#pragma unroll
+      for (int j = 0; j < 2; ++j) {
+        int cur_src_sparse = 0;
+        src_sparse[0 + (2 * j)] = 0;
+        src_sparse[1 + (2 * j)] = 0;
+        for (int i = 0; i < 4; ++i) {
+          if (src[i + (4 * j)] != (half)0.0f) {
+            src_sparse[cur_src_sparse + (2 * j)] = src[i + (4 * j)];
+            *cur_meta |= i << (6 - (2 * (1 - cur_src_sparse) + (4 * j)));
+
+            cur_src_sparse++;
+          }
+        }
+      }
+      *(metadata + (blockRow * colRegions + i * MMA_M * 2 + lane_id)) =
+          *cur_meta;
+
+      *(int2 *)(sparseMatrixA +
+                (blockRow * colRegions + i * MMA_M * (MMA_K / 2) + lane_id)) =
+          *(int2 *)src_sparse;
+
+      *((int2 *)(&A_smem[lane_id / 2][0]) + lane_id % 2) =
+          *((int2 *)src_sparse);
+    }
+  }
+}
+
+__global__ void mmaSTKernelSparse(half *bcsrValuesA, char *metadata,
+                                  half *sparseMatrixA, half *B, half *C,
+                                  size_t M, size_t N, size_t K,
+                                  size_t nonzeroBlocks, int *blockInfo,
                                   int *relativeBlockIndexMapping) {
   int PRINT_THREAD_ID = 11;
   // mmaSTKernel
@@ -110,39 +200,12 @@ __global__ void mmaSTKernelSparse(half *bcsrValuesA, half *B, half *C, size_t M,
       __shared__ half B_smem[MMA_N][MMA_K];
       __shared__ char Meta_smem[MMA_M][MMA_K / 8];
 
-      half *src = // length 8
-          ((half *)((int4 *)(&bcsrValuesA[(relativeIndex)*MMA_M * MMA_K +
-                                          (lane_id / 2) * MMA_K]) +
-                    lane_id % 2));
-
-      half src_sparse[4];
-
-      char *cur_meta = (Meta_smem[lane_id / 2]) + (lane_id % 2);
-
-      *cur_meta = 0;
-
-#pragma unroll
-      for (int j = 0; j < 2; ++j) {
-        int cur_src_sparse = 0;
-        src_sparse[0 + (2 * j)] = 0;
-        src_sparse[1 + (2 * j)] = 0;
-        for (int i = 0; i < 4; ++i) {
-          if (src[i + (4 * j)] != (half)0.0f) {
-            src_sparse[cur_src_sparse + (2 * j)] = src[i + (4 * j)];
-            *cur_meta |= i << (6 - (2 * (1 - cur_src_sparse) + (4 * j)));
-            // *cur_meta |= i << (2 * cur_src_sparse + (4 * j));
-            cur_src_sparse++;
-          }
-        }
-        // all zeroes
-        // if (cur_src_sparse == 0) {
-        //   *cur_meta |= 0b0100 << (4 * j);
-        //   // *cur_meta |= 0b0100 << (6 - 4 * j);
-        // }
-      }
-
       *((int2 *)(&A_smem[lane_id / 2][0]) + lane_id % 2) =
-          *((int2 *)src_sparse);
+          *(int2 *)(sparseMatrixA + (blockRow * colRegions +
+                                     i * MMA_M * (MMA_K / 2) + lane_id));
+
+      *((Meta_smem[lane_id / 2]) + (lane_id % 2)) =
+          *(metadata + (blockRow * colRegions + i * MMA_M * 2 + lane_id));
 
       if (lane_id < MMA_N * 2) {
         *((int4 *)(&B_smem[lane_id / 2][0]) + lane_id % 2) =
@@ -193,12 +256,26 @@ __global__ void mmaSTKernelSparse(half *bcsrValuesA, half *B, half *C, size_t M,
   }
 }
 
-void mmaSTKernel(half *bcsrValuesA, half *B, half *C, size_t M, size_t N,
-                 size_t K, size_t nonzeroBlocks, int *blockInfo,
+void mmaSTKernel(half *bcsrValuesA, char *metadata, half *sparseMatrixA,
+                 half *B, half *C, size_t M, size_t N, size_t K,
+                 size_t nonzeroBlocks, int *blockInfo,
                  int *relativeBlockIndexMapping) {
   dim3 block(WARP_SIZE);
   dim3 grid(div_ceil(N, MMA_N), div_ceil(M, MMA_M));
 
-  mmaSTKernelSparse<<<grid, block>>>(bcsrValuesA, B, C, M, N, K, nonzeroBlocks,
-                                     blockInfo, relativeBlockIndexMapping);
+  mmaSTKernelSparse<<<grid, block>>>(bcsrValuesA, metadata, sparseMatrixA, B, C,
+                                     M, N, K, nonzeroBlocks, blockInfo,
+                                     relativeBlockIndexMapping);
+}
+
+void preprocessing_mmaSTKernel(half *bcsrValuesA, char *metadata,
+                               half *sparseMatrixA, size_t M, size_t N,
+                               size_t K, size_t nonzeroBlocks, int *blockInfo,
+                               int *relativeBlockIndexMapping) {
+  dim3 block(WARP_SIZE);
+  dim3 grid(div_ceil(N, MMA_N), div_ceil(M, MMA_M));
+
+  preprocessing_mmaSTKernelSparse<<<grid, block>>>(
+      bcsrValuesA, metadata, sparseMatrixA, M, N, K, nonzeroBlocks, blockInfo,
+      relativeBlockIndexMapping);
 }
